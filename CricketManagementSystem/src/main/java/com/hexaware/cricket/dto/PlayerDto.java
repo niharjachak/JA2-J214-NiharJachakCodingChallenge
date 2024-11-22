1f327ab9c9d5e2dfb1d9fd7f0b6d0019cfc3cfe5
Nihar <nihar.jachak@gmail.com>
2024-11-22 18:00:33 +0530

    Added Playerdto

---
+++ CricketManagementSystem/src/main/java/com/hexaware/cricket/dto/PlayerDto.java
@@ -0,0 +1,111 @@
+package com.hexaware.cricket.dto;
+
+public class PlayerDto {
+	
+	private int playerId;
+	
+	private String playerName;
+	
+	private int jerseyNumber;
+	
+	private String role;
+	
+	private long totalMatches;
+	
+	private String teamName;
+	
+	private String state;
+	
+	private String description;
+
+	public PlayerDto() {
+		super();
+	}
+
+	public PlayerDto(int playerId, String playerName, int jerseyNumber, String role, long totalMatches, String teamName,
+			String state, String description) {
+		super();
+		this.playerId = playerId;
+		this.playerName = playerName;
+		this.jerseyNumber = jerseyNumber;
+		this.role = role;
+		this.totalMatches = totalMatches;
+		this.teamName = teamName;
+		this.state = state;
+		this.description = description;
+	}
+
+	public int getPlayerId() {
+		return playerId;
+	}
+
+	public void setPlayerId(int playerId) {
+		this.playerId = playerId;
+	}
+
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+
+	public int getJerseyNumber() {
+		return jerseyNumber;
+	}
+
+	public void setJerseyNumber(int jerseyNumber) {
+		this.jerseyNumber = jerseyNumber;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public long getTotalMatches() {
+		return totalMatches;
+	}
+
+	public void setTotalMatches(long totalMatches) {
+		this.totalMatches = totalMatches;
+	}
+
+	public String getTeamName() {
+		return teamName;
+	}
+
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "PlayerDto [playerId=" + playerId + ", playerName=" + playerName + ", jerseyNumber=" + jerseyNumber
+				+ ", role=" + role + ", totalMatches=" + totalMatches + ", teamName=" + teamName + ", state=" + state
+				+ ", description=" + description + "]";
+	}
+
+	
+	
+}
